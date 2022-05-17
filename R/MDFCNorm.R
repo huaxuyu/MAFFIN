ba374c09dc3af4cbc333dbac87dec0e8d2415cf1
Waddles <yhxchem@outlook.com>
2022-05-17 16:05:36 -0700

    Update bandwidth optimization.

--- R/MDFCNorm.R
+++ R/MDFCNorm.R
@@ -78,6 +78,9 @@ MDFCNorm = function(FeatureTable, IntThreshold=0, SampleInCol=TRUE, output=FALSE
   FeatureTable_index = (2:ncol(FeatureTable))[c(temp)]
   group_vector = as.character(FeatureTable[1,FeatureTable_index])
 
+  if (sum(table(group_vector) != 0)) {
+    bwOpt = 0.25
+  }
 
   # Only use high-quality features if labeled
   quality_exist = !is.null(FeatureTable$Quality)
